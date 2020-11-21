01bcceaab6e3b1faf10c184aea8ce6e991f3adcd
tvylormvde <wernerowy@gmail.com>
2020-11-21 18:21:53 +0100

    Implement config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+
+DIRECTORY = "report"
+URL = "https://api.spotify.com/v1/recommendations?"
+TOKEN = "BQDpJR9AIppzfZ-POlB6PJV7AJEM8CkHZuv8FPC8zjmyvrAQpx6OJVE1wvB4RsGeNztJuQCB0391BvUV14ymv5Lwk29z38yp5OTEnPD2Y3QI8L_f49ThoD11HFR5jWzL6QnJyo7yAhHNFz5oxQI2RhUnK8IkQY6zqE2m76FUNPSFF8Q8krL09KenQob6HrR0SypOOvrloXne"
+FILTERS = {
+    "limit": 10,
+    "market": "US",
+    "seed_genres": "indie",
+}
