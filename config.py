eb83230d0bde8e18de3244ae9cc8a9221047e211
tvylormvde <wernerowy@gmail.com>
2020-11-22 17:03:49 +0100

    Fix implementation

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-
-DIRECTORY = "report"
-URL = "https://api.spotify.com/v1/recommendations?"
-TOKEN = "BQDpJR9AIppzfZ-POlB6PJV7AJEM8CkHZuv8FPC8zjmyvrAQpx6OJVE1wvB4RsGeNztJuQCB0391BvUV14ymv5Lwk29z38yp5OTEnPD2Y3QI8L_f49ThoD11HFR5jWzL6QnJyo7yAhHNFz5oxQI2RhUnK8IkQY6zqE2m76FUNPSFF8Q8krL09KenQob6HrR0SypOOvrloXne"
-FILTERS = {
-    "limit": 10,
-    "market": "US",
-    "seed_genres": "indie",
+TOKEN = "Your Token"
+USER_ID = "Your Spotify ID"
+ARTIST_ID = "Artist ID"
+MARKET = "US"
+PLAYLIST_INFO = {
+    "name": "Playlist name",
+    "description": "Playlist description",
+    "public": False
 }
